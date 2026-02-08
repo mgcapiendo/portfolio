51130669d9b418aa6307092006a959413b677820
Manuel Capiendo <mgcapiendo2023@fau.edu>
2026-02-08 18:13:48 -0500

    okay im liking this so far, we have the islands moving, a section for the about, and a new vision...i think

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,44 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
+import { useEffect } from 'react';
+
+
 const App = () => {
+useEffect(() => {
+  function handleScroll() {
+    const scrollY = window.scrollY;
+    const vh = window.innerHeight;
+    const p = Math.min(scrollY / vh, 1);
+
+    const mt1 = document.querySelector('.mountain');
+    const mt2 = document.querySelector('.mountain2');
+    const ab1 = document.querySelector('.about1');
+    const ab2 = document.querySelector('.about2');
+
+    if (mt1) mt1.style.transform = `translateX(${p * 80}vw)`;
+    if (mt2) mt2.style.transform = `translateX(-${p * 80}vw)`;
+    if (ab1) ab1.style.transform = `translateX(-${p * 80}vw)`;
+    if (ab2) ab2.style.transform = `translateX(${p * 80}vw)`;
+
+    // const about = document.querySelector('.about');
+
+    // if (about) about.style.transform = `translateY(${p * 80}vw)`;
+  }
+
+  window.addEventListener('scroll', handleScroll);
+  return () => window.removeEventListener('scroll', handleScroll);
+}, []);
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <About />
-      <Fossil />
+      {/* <Fossil />
       <Experience />
       <Projects />
-      <Contact />
+      <Contact /> */}
     </div>
   )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,23 @@
+import about1 from "../assets/about1.svg";
+import about2 from "../assets/about2.svg";
+
 const About = () => {
     return (
         <div className="about">
-            <h1>THIS IS ABOUT</h1>
-            <h2>LOREM IPSUM SOMETHING SOMETHING</h2>
+            <img src={about1} className="about1"/>
+            <img src={about2} className="about2"/>
+            
+            <div className="about-container">
+                <h1>About</h1>
 
-            <h2>Hey! I'm Manuel, an aspiring software engineer, and I want to build technology that inspires and creates wonder. 
-            My journey began in middle school with an engineering program and has continued through college, 
-            where I study computer science and artificial intelligence. 
-            Along the way, my interests in process improvement and enterprise design have taught me to adapt, learn quickly, 
-            and collaborate with great people. I'm determined to contribute to projects that push technology forward 
-            while exploring opportunities to learn, grow, and make the most impact.
-            </h2>
+                <h2>Hey! I'm Manuel, an aspiring software engineer, and I want to build technology that inspires and creates wonder. 
+                My journey began in middle school with an engineering program and has continued through college, 
+                where I study computer science and artificial intelligence. 
+                Along the way, my interests in process improvement and enterprise design have taught me to adapt, learn quickly, 
+                and collaborate with great people. I'm determined to contribute to projects that push technology forward 
+                while exploring opportunities to learn, grow, and make the most impact.
+                </h2>
+            </div>
         </div>
     )
 }
