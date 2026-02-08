eac0a7f8b10648045b109e2af27d726e17e02002
Manuel Capiendo <mgcapiendo2023@fau.edu>
2026-02-08 13:14:51 -0500

    idk what i changed bc i just spent so long trying to add figma stuff and i hate it so im uploading this and going to get food

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
-
 const App = () => {
   return (
     <div className="App">
