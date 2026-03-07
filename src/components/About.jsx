1c3ef65d9ff7e5ebbba2ca9e2dd2d4d0783da5bb
Manuel Capiendo <mgcapiendo2023@fau.edu>
2026-03-07 15:37:48 -0500

    skeleton is getting more bones

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,13 +13,11 @@ const About = () => {
                 <h2>Hey! I'm Chok, an aspiring software engineer, and I want to build technology that inspires and creates wonder. 
                 My journey began in middle school and high school with an engineering program, then continued through college ROTC, and now
                 where I am pursuing my degree in computer science and minor in artificial intelligence. 
-                {/* Along the way, my interests in process improvement and enterprise design have taught me to adapt, learn quickly, 
+                Along the way, my interests in process improvement and enterprise design have taught me to adapt, learn quickly, 
                 and collaborate with great people. I'm determined to contribute to projects that push technology forward 
-                while exploring opportunities to learn, grow, and make the most impact. */}
+                while exploring opportunities to learn, grow, and make the most impact.
                 </h2>
             </div>
-
-            
         </div>
     )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,30 @@
 const Contact = () => {
     return (
         <div className="contact">
-            THIS WILL BE CONTACT SECTION SEND SIGNAL FLARE
+
+            <div className="contact-card">
+                <h1>Contact</h1>
+                
+                <div className="contact-bubbles">
+
+                    <div className="socials">
+                        <div className="bubble">
+                        <h3>Linkedin</h3>
+
+                        </div>
+                        <div className="bubble">
+                            <h3>GitHub</h3>
+                        </div>
+                        <div className="bubble-email">
+                            <h3>manuelgcapiendo@gmail.com</h3>
+                        </div>
+                    </div>
+
+                    <div className="resume">
+                        <h3>View Full Resume</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
